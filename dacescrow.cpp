ff94bda4694075e7fa5b767abc5eea2b24041e6b
Khazius <nsrempel@gmail.com>
2019-04-09 16:11:05 +1000

    Change to BOS token

--- dacescrow.cpp
+++ dacescrow.cpp
@@ -52,7 +52,7 @@ namespace eosdac {
     ACTION dacescrow::init(name sender, name receiver, name arb, time_point_sec expires, string memo, std::optional<uint64_t> ext_reference ) {
         require_auth(sender);
 
-        extended_asset zero_asset{{0, symbol{"EOS", 4}}, "eosio.token"_n};
+        extended_asset zero_asset{{0, symbol{"BOS", 4}}, "eosio.token"_n};
 
         auto by_sender = escrows.get_index<"bysender"_n>();
 
